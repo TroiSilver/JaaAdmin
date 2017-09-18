59b01eb368229aa65a1309fd3b637fc679ca2704
TROi <UBITRADEX@GMAIL.COM>
2017-09-18 23:28:18 +0200

    ADDED FILES

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'core/init.php';
+// echo Config::get('remember/cookie_expiry') ,"<br />";
+// echo Config::get('remember/cookie_name'),"<br />";
+// echo Config::get("mysql/db"),"<br />";
+// echo Config::get('mysql/username'),"<br />";
+// echo Config::get('mysql/host'),"<br />";
+// echo "<hr />";
+//-----------------------------------------------------------
+
+DB::getInstance();
+// ?>
+ <?php
+// $link = mysqli_connect("127.0.0.1", "root", "root", "jaaschool");
+//
+// if (!$link) {
+//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
+// }
+//
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+//
+// mysqli_close($link);
+?>
